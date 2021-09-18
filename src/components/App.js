b2397da4c3bcf13e9adb690e50116d286e1cf5fd
Sonu Ram <sonuram6960@gmail.com>
2021-09-18 17:55:58 +0530

    Take components out of timer folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "../styles/main.scss";
-import { Timer } from "./timer/Timer";
+import { Timer } from "./Timer";
 
 export const App = () => {
   return <Timer />;
